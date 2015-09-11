e113aff82f9851e5fa8564440b76beb0010dff01
dbjohnson <d.bryan.johnson@gmail.com>
2015-09-11 11:48:51 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # python-utils
+Commonly-used utilities and helpers
+
+##db.py
+Postgres database connection manager, backed by a threaded connection pool.
+
+Usage:
+
+```python
+from flutil import db
+
+db_url = 'postgres://foo:bar@baz:port/db_name'
+con = db.PoolManager(db_url)
+
+query = 'select * from foo where bar = (?)'
+with con.cursor() as c:
+	c.execute(query, (bar,))
+
+statement = 'insert into foo (bar, baz) values (?, ?)'
+with con.cursor(commit_on_close=True) as c:
+	c.execute(statement, (bar, baz))
+```
---
+++ flutil/db.py
@@ -0,0 +1,41 @@
+import logging
+import psycopg2
+from psycopg2 import pool
+import atexit
+from contextlib import contextmanager
+
+
+MAX_CONNECTION_ATTEMPTS = 10
+
+
+class PoolManager:
+    def __init__(self, connection_url, name=None, mincount=2, maxcount=40, cursor_factory=None):
+        self._pool = pool.ThreadedConnectionPool(mincount, maxcount, connection_url, cursor_factory=cursor_factory)
+        self.name = name or connection_url
+
+    def __del__(self):
+        self._pool.closeall()
+
+    @contextmanager
+    def cursor(self, commit_on_close=False):
+        for _ in range(MAX_CONNECTION_ATTEMPTS):
+            try:
+                con = self._pool.getconn()
+                if not con.closed:
+                    break
+            except psycopg2.DatabaseError, psycopg2.OperationalError:
+                pass
+        else:
+            raise RuntimeError('Could not get a connection to: {}'.format(self.name))
+
+        try:
+            yield con.cursor()
+            if commit_on_close:
+                con.commit()
+        except pool.PoolError as e:
+            logging.log(logging.ERROR, e.message)
+        finally:
+            try:
+                self._pool.putconn(con)
+            except psycopg2.pool.PoolError as e:
+                logging.warn(e.message)
