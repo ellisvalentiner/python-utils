000d16f7b2dd131d5cb294e436825b175e71ea54
Michael Whalen <mwhalen@farmlogs.com>
2015-09-14 12:56:46 -0400

    Added a from_name convenience helper to simplify db creation.

--- flutil/db.py
+++ flutil/db.py
@@ -1,14 +1,30 @@
+import os
 import logging
 import psycopg2
+
 from psycopg2 import pool
 from psycopg2.extras import RealDictCursor
 from contextlib import contextmanager
 
-
 MAX_CONNECTION_ATTEMPTS = 10
 
-
 class PoolManager:
+    @staticmethod
+    def get_url_from_environment(name=None):
+        # Default to nothing for the userdata database url, which is just DATABASE_URL
+        env = 'DATABASE_URL'
+        if name:
+            env = '_'.join([name.upper(), 'DATABASE_URL'])
+
+        return os.environ.get(env, None), env
+
+    @classmethod
+    def from_name(cls, name=None):
+        url, env = cls.get_url_from_environment(name)
+        if not url:
+            assert "Please specify a valid database, or ensure %s exists in your environment." % (env, )
+        return cls(connection_url=url, name=env)
+
     def __init__(self, connection_url, name=None, mincount=2, maxcount=40,
                  cursor_factory=RealDictCursor):
         self._pool = pool.ThreadedConnectionPool(mincount, maxcount, connection_url, cursor_factory=cursor_factory)
